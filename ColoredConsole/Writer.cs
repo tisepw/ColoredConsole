9e52e097bc9d5f70a5768e8c645a5bfbee3e14e4
Vlad_Den <33760265+VladDen4@users.noreply.github.com>
2023-03-28 16:54:33 +0300

    Add a license comment to files

--- ColoredConsole/Writer.cs
+++ ColoredConsole/Writer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Vlad_Den <vladden500@gmail.com>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
 
 namespace ColoredConsole
 {
